4594f07c263230f7491a74c9a51f799fdcc23ec6
Juan Leiton <juandiegoleitonpena@gmail.com>
2026-08-27 08:58:50 -0500

    Feat: Agrego clase plato y mesa

---
+++ src/gestionproyectos/Mesa.java
@@ -0,0 +1,14 @@
+public class Mesa {
+
+    private double Medida;
+    private String Material;
+    private String Forma;
+    private String Color;
+
+    public Mesa(String Medida, String Material, String Forma, String Color) {
+        this.Color = Color;
+        this.Forma = Forma;
+        this.Medida = Medida;
+        this.Material = Material;
+    }
+}
---
+++ src/gestionproyectos/Plato.java
@@ -0,0 +1,14 @@
+public class Plato {
+
+    private double Peso;
+    private String Material;
+    private String Forma;
+    private String Color;
+
+    public Plato(String Peso, double Peso, String Forma, String Color) {
+        this.Color = Color;
+        this.Forma = Forma;
+        this.Peso = Peso;
+        this.Material = Material;
+    }
+}
